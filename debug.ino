1fe80ceda11c95d1940abb9d454887284f235084
Bruno <bga@gmail.com>
2021-03-30 09:42:20 -0700

    Add simple real example.

--- debug.ino
+++ debug.ino
@@ -1,1 +1,11 @@
-// Dummy file just so Arduino IDE / VScode are happy.
+#include <blinklib.h>
+
+#include "debug.h"
+
+void setup() {}
+
+void loop() {
+  // Message will be sent to serial debugging port only if debug is enabled.
+  // Otherwise this line will not even be compiled in.
+  LOGFLN("Debug is enabled");
+}
